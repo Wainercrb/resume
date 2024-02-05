60f15c5f7d9f6ef6a38c43b79d51884652848d2e
wainerrodriguez <weiner.rodriguez@costaricasoftwareservices.com>
2024-02-05 14:36:45 -0600

    Improved skills layout

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,10 +6,10 @@ interface IProps {
 
 function Skills({ skills = [] }: IProps) {
   return (
-    <ul className="flex flex-wrap gap-2 my-3">
+    <ul className="flex flex-wrap gap-2 mb-3 mt-4 ml-6">
       {skills.map(({ name }, idx) => (
         <li key={idx}>
-          <div className="rounded-md border border-gray-900 py-0 px-1 font-mono text-xs font-bold text-gray-700 w-fit">
+          <div className="rounded-md border border-gray-900 py-0 px-1 font-mono text-xs font-bold text-gray-700 w-fit bg-neutral-200">
             <span className="tag">{name}</span>
           </div>
         </li>
